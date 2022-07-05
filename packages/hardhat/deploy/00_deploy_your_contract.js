ea4ee3c9f98944e85d8e28af86f6b88e3c126d1a
Carlos Sánchez <oceanrdn@gmail.com>
2022-07-05 11:16:44 +0200

    Config file with NFT levels. Add levels dynamically on the deploy script

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -1,18 +1,13 @@
 // deploy/00_deploy_your_contract.js
 const { ethers } = require("hardhat");
+// JuiceBox config
+const config = require("../../../config.json");
 
 const localChainId = "31337";
 
-// JuiceBox config
-const juiceBoxProjectId = "4283";
-// ToDo. Maybe this should go the global config.json we are going to create?
-// We have deployed these Payers from the JB UI
-const juiceboxPayers = {
-  1: "",
-  4: "0x7c89c6a1686109D7514aD009BAD27140EC94fc80",
-};
-
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  // ToDo. Check that config.json is filled
+
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = await getChainId();
@@ -28,22 +23,29 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     const dummyPayer = await ethers.getContract("DummyPayer", deployer);
     juiceBoxPayerAddress = dummyPayer.address;
   } else {
-    juiceBoxPayerAddress = juiceboxPayers.chainId;
+    juiceBoxPayerAddress = config.juiceboxPayersAddresses.chainId;
   }
 
   await deploy("JBNFT", {
     from: deployer,
-    args: [juiceBoxProjectId, juiceBoxPayerAddress],
+    args: [config.juiceBoxProjectId, juiceBoxPayerAddress],
     log: true,
     waitConfirmations: 5,
   });
 
   const jbnft = await ethers.getContract("JBNFT", deployer);
 
-  const tx = await jbnft.addLevel(
-    ethers.utils.parseEther("0.01"),
-    "QmfVMAmNM1kDEBYrC2TPzQDoCRFH6F5tE1e9Mr4FkkR5Xr"
+  let tx;
+  await Promise.all(
+    config.nfts.levels.map(async (level) => {
+      // ToDo. We could have the constructor getting the initial levels.
+      tx = await jbnft.addLevel(
+        ethers.utils.parseEther(level.price),
+        level.metadataHash
+      );
+
+      await tx.wait();
+    })
   );
-  await tx.wait();
 };
 module.exports.tags = ["JBNFT"];
